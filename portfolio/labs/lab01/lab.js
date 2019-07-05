cfc209ba066c5866d1ffb09f35ad472cffb94a3e
star041500 <52520239+star041500@users.noreply.github.com>
2019-07-05 17:40:58 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,7 +4,38 @@
 
 
 function start(e) {
+     var name=[
+     {
+        char: '劉',
+        big5: 'BC12',
+        cns: '1-6C2C',
+        unicode: '5289'
+     },
+     {
+        char: '冠',
+        big5: 'AB61',
+        cns: '1-5025',
+        unicode: '51A0'
+     },
+     {
+        char: '宏',
+        big5: 'A7BB',
+        cns: '1-497B',
+        unicode: '5B8F'
+     }
+     ];
+     var skr=document.getElementById("SKR");
 
+     for(var i=0; i<3; i++){
+        skr.innerHTML+=name[i].char;
+        skr.innerHTML+=' big5= ';
+        skr.innerHTML+=name[i].big5;
+        skr.innerHTML+=' cns= ';
+        skr.innerHTML+=name[i].cns;
+        skr.innerHTML+=' unicode= ';
+        skr.innerHTML+=name[i].unicode;
+        skr.innerHTML+='<br>';
+     }
 }
 
 /*
--- portfolio/labs/lab02/lab.js
+++ portfolio/labs/lab02/lab.js
@@ -1,14 +1,27 @@
 /*
 
 */
-
-
-function start(e) {
-
+function update(){
+    var x=document.getElementById('ch').value;
+    var ele=document.getElementById('ch2');
+    var y='';
+    for (var i = 0;i< x.length; i++){
+      y+=x.charAt(i)
+      y+=' U+ ';
+      y+=x.charCodeAt(i).toString(16).toUpperCase();
+      y+='<br>';
+    } 
+    ele.innerHTML=y ;
+    document.getElementById('ch1').innerHTML=
+    document.getElementById('ch').value.replace('貓','娘')
 }
 
+function start(e) {
+    document.getElementById('ch').addEventListener("input", update);
+    update();
+}
 /*
-https://www.w3schools.com/jsref/met_document_addeventlistener.asp
+++www.w3schools.com/jsref/met_document_addeventlistener.asp
 
 document.addEventListener(event, function, useCapture)
 
